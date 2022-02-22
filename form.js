5e066b836aad523a7f4a824168c27dd86583e27a
Rupsi Mehta <rupsi.mehta@comprotechnologies.com>
2022-02-22 10:01:00 +0530

    first commit

---
+++ form.js
@@ -0,0 +1,20 @@
+var express = require("express");
+const Port = process.env.PORT || 8000;
+var sampleFile;
+var uploadPath;
+const open = require('open');
+//use the application off of express.
+var app = express();
+
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + "/index1.html");
+});
+
+
+app.listen(Port, function () {
+    console.log('Express server listening on port ', Port); // eslint-disable-line
+});
+//define the route for "/"
+app.listen(8080);
+open('http://localhost:8080');
+console.log("Please launch http://localhost:8080 in your browser url");
