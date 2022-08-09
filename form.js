8dbd9989bd493f7ea8f3671dfbc66a6e49f4e594
Rupsi Mehta <rupsi.mehta@comprotechnologies.com>
2022-08-09 13:07:05 +0530

    check

--- form.js
+++ form.js
@@ -19,7 +19,9 @@ app.get("/", function (request, response) {
 app.listen(Port, function () {
     console.log('Express server listening on port ', Port); // eslint-disable-line
 });
-
+app.listen(8080);
+open('http://localhost:8080');
+console.log("Please launch http://localhost:8080 in your browser url/ or direct url");
 app.get('/homepage', function (request, response) {
     response.sendFile(__dirname + "/home.html");
 }),
@@ -51,44 +53,79 @@ app.post('/upload', function (req, res) {
                 var loJson = JSON.parse(data);
                 quizObj = {
                     "quizName": {
-                        "totalScore": 0,
-                        "passScore": 0,
+                        // "totalScore": 0,
+                        // "passScore": 0,
+                        "type": "Native Engage Learning Object",
+                        "name": "",
                         "question": []
                     }
                 }
                 Object.keys(loJson.itemBody.questions).forEach(function (qindex) {
                     quizObj.quizName.question[qindex] = {
-                        "id": "",
-                        "number": "",
-                        "type": "",
-                        "title": "",
-                        "instruction": "",
-                        "prompt": "",
-                        "media": undefined,
-                        "maxScore": "",
-                        //"maxOptions": "",
+                        "number": null,
+                        "quesType": "",
+                        "quesTitle": "",
+                        "instructionText": null,
+                        "promptText": null,
+                        "viewLabels": {
+                            "instructionHeading": null,
+                            "selectOneLabel": null,
+                            "selectOneOrMoreLabel": null,
+                            "selectFromDropdownLabel": null,
+                            "matchingLeftLabel": null,
+                            "matchingRightLabel": null,
+                            "zoomLabel": null,
+                            "selectLabel": null,
+                            "noneLabel": null,
+                            "clearLabel": null
+                        },
+                        "mediaType": null,
+                        "subQues": [],
                         "answerKey": [],
                         "scenario1": [],
                         "scenario2": [],
-                        "viewLabels": undefined
+                        // "maxScore": "",
+                        //"maxOptions": "",
+                        // "id": "",
                     }
-                    quizObj.quizName.question[qindex].id = loJson.itemBody.questions[qindex].id;
+                    //quizObj.quizName.question[qindex].id = loJson.itemBody.questions[qindex].id;
+            
+                    console.log(loJson.itemBody.questions[qindex].id);
+            
                     quizObj.quizName.question[qindex].number = parseInt(qindex) + 1;
-                    quizObj.quizName.question[qindex].maxScore = loJson.itemBody.questions[qindex].meta.score;
-                    var quesJson = loJson.itemFragments[quizObj.quizName.question[qindex].id];
-                    quizObj.quizName.question[qindex].type = quesJson.data.player;
-                    quizObj.quizName.question[qindex].title = quesJson.data.itemBody.stimulus.content.data.replace(/(<([^>]+)>)/ig, '');
-                    quizObj.quizName.question[qindex].instruction = quesJson.data.itemBody.instruction.content.data.replace(/(<([^>]+)>)/ig, '');
-                    quizObj.quizName.question[qindex].prompt = quesJson.data.itemBody.prompt.content.data.replace(/(<([^>]+)>)/ig, '');
-                    quizObj.quizName.question[qindex].media = quesJson.data.itemBody.media.content.data;
+                    quizObj.quizName.name = loJson.itemMeta.title;
+                    var quesJson = loJson.itemFragments[loJson.itemBody.questions[qindex].id];
+                    quizObj.quizName.question[qindex].quesType = quesJson.data.player;
+                    quizObj.quizName.question[qindex].quesTitle = quesJson.data.itemBody.stimulus.content.data.replace(/(<([^>]+)>)/ig, '');
+                    quizObj.quizName.question[qindex].instructionText = quesJson.data.itemBody.instruction.content.data.replace(/(<([^>]+)>)/ig, '') == "" ? null : quesJson.data.itemBody.instruction.content.data.replace(/(<([^>]+)>)/ig, '');
+                    if (quesJson.data.itemBody.prompt != null)
+                        quizObj.quizName.question[qindex].promptText = (quesJson.data.itemBody.prompt.content.data.replace(/(<([^>]+)>)/ig, '')) == "" ? null : quesJson.data.itemBody.prompt.content.data.replace(/(<([^>]+)>)/ig, '');
+            
+                    quizObj.quizName.question[qindex].mediaType = (quesJson.data.itemBody.media.content.data.type) == "" ? null : (quesJson.data.itemBody.media.content.data.type);
                     quizObj.quizName.question[qindex] = getQuesdetail(quesJson, quizObj.quizName.question[qindex]);
-                    quizObj.quizName.totalScore = quizObj.quizName.totalScore + quizObj.quizName.question[qindex].maxScore;
-                    //quizObj.quizName.question[qindex].id = undefined;
-                    quizObj.quizName.question[qindex].scenario1 = quizObj.quizName.question[qindex].answerKey;
-                    quizObj.quizName.question[qindex].scenario2 = quizObj.quizName.question[qindex].answerKey;
+                    //quizObj.quizName.question[qindex].scenario1 = quizObj.quizName.question[qindex].answerKey;
+                    //quizObj.quizName.question[qindex].scenario2 = quizObj.quizName.question[qindex].answerKey;
+                    quizObj.quizName.question[qindex].subQues = quizObj.quizName.question[qindex].subQues;
+            
+                    if (quesJson.data.itemBody.viewLabels != null) {
+                        quizObj.quizName.question[qindex].viewLabels = {
+                            instructionHeading: quesJson.data.itemBody.viewLabels.instructionHeading || null,
+                            selectOneLabel: quesJson.data.itemBody.viewLabels.selectOne || null,
+                            selectOneOrMoreLabel: quesJson.data.itemBody.viewLabels.selectOneOrMore || null,
+                            selectFromDropdownLabel: quesJson.data.itemBody.viewLabels.selectFromDropdown || null,
+                            matchingLeftLabel: quesJson.data.itemBody.viewLabels.matchingLeftLabel || null,
+                            matchingRightLabel: quesJson.data.itemBody.viewLabels.matchingRightLabel || null,
+                            zoomLabel: quesJson.data.itemBody.viewLabels.zoomLabel || null,
+                            selectLabel: quesJson.data.itemBody.viewLabels.selectLabel || null,
+                            noneLabel: quesJson.data.itemBody.viewLabels.noneLabel || null,
+                            clearLabel: quesJson.data.itemBody.viewLabels.clearLabel || null,
+                        }
+                    }
+                    // quizObj.quizName.totalScore = quizObj.quizName.totalScore + quizObj.quizName.question[qindex].maxScore;
+                    // quizObj.quizName.question[qindex].maxScore = loJson.itemBody.questions[qindex].meta.score;
                 })
-                quizObj.quizName.passScore = Math.round(quizObj.quizName.totalScore * 0.7 * 10) / 10;
-                fs.writeFile(__dirname + '/download/' + sampleFile.name , jsonFormat(quizObj, {
+               // quizObj.quizName.passScore = Math.round(quizObj.quizName.totalScore * 0.7 * 10) / 10;
+                fs.writeFile(__dirname + '/download/' + "output.json" , jsonFormat(quizObj, {
                     type: 'space',
                     size: 2
                 }), function (err) {
@@ -101,8 +138,8 @@ app.post('/upload', function (req, res) {
 app.get('/single', function (req, res) {
     console.log('single file');
     // Download function provided by express
-    console.log(sampleFile.name)
-    res.download(__dirname + '/download/' + sampleFile.name  , function (err) {
+    //console.log(sampleFile.name)
+    res.download(__dirname + '/download/' + "output.json"  , function (err) {
         if (err) {
             console.log(err);
         }
@@ -110,9 +147,7 @@ app.get('/single', function (req, res) {
 })
 
 //define the route for "/"
-app.listen(8080);
-open('http://localhost:8080');
-console.log("Please launch http://localhost:8080 in your browser url/ or direct url");
+
 function getQuesdetail(qJson, obj) {
     var quesOptions;
     if (qJson.data.itemBody.options != undefined)
@@ -126,21 +161,42 @@ function getQuesdetail(qJson, obj) {
     //obj.maxOptions = quesOptions.length;
     var quesValidations = qJson.data.itemBody.validations;
 
-    if (obj.type == 'multi-mcqsr-itemplayer') {
+    if (obj.quesType == 'multi-mcqsr-itemplayer' || obj.quesType == 'mcqmr-itemplayer') {
         Object.keys(qJson.data.itemBody.questions).forEach(function (i) {
             let quesOptions = [];
             let answerKey = [];
-            quesOptions[i] = qJson.data.itemBody.questions[i].data.options;
+            let subQues = [];
+            let subQuesOption = [];
+            subQuesOption[i] = qJson.data.itemBody.questions;
+            Object.keys(subQuesOption).forEach(function (j) {
+                subQues[j] = [];
+                subQues[j][0] = subQuesOption[i][j].id.substring(13);
+                subQues[j][1] = (subQuesOption[i][j].data.stimulus.content.data.replace(/(<([^>]+)>)/ig, '')) == "" ? null : (subQuesOption[i][j].data.stimulus.content.data.replace(/(<([^>]+)>)/ig, ''));
+                subQues[j][2] = (subQuesOption[i][j].data.media.content.data.type) == "" ? null : subQuesOption[i][j].data.media.content.data.type;
+                obj.subQues.push(subQues[j])
+            })
+
+            quesOptions[i] = qJson.data.itemBody.questions[i].data
+                .options;
             Object.keys(quesOptions[i]).forEach(function (j) {
                 answerKey[j] = [];
                 answerKey[j][0] = quesOptions[i][j].id;
-                answerKey[j][1] = quesOptions[i][j].content.data;
-                answerKey[j][2] = "";
-                answerKey[j][3] = "";
+                answerKey[j][1] = (quesOptions[i][j].content.data.replace(/(<([^>]+)>)/ig, '')) == "" ? null : quesOptions[i][j].content.data.replace(/(<([^>]+)>)/ig, '')
+                answerKey[j][2] = null;
+                answerKey[j][3] = null;
+                answerKey[j][4] = (quesOptions[i][j].media.content.data.type) == "" ? null : quesOptions[i][j].media.content.data.type;
                 Object.keys(qJson.data.itemBody.validations).forEach(function (key) {
-                    if (qJson.data.itemBody.questions[i].id == quesValidations[key].responseContainer && quesValidations[key].correctResponse == answerKey[j][0]) {
-                        answerKey[j][2] = "select";
-                        answerKey[j][3] = "correct";
+                    if (obj.quesType == 'mcqmr-itemplayer') {
+                        if (qJson.data.itemBody.questions[i].id == quesValidations[key].responseContainer.slice(0, -3) && quesValidations[key].correctResponse == "true" && answerKey[j][0] == quesValidations[key].responseContainer.substring(15)) {
+                            answerKey[j][2] = "select";
+                            answerKey[j][3] = "correct";
+                        }
+                    }
+                    else if (obj.quesType == 'multi-mcqsr-itemplayer') {
+                        if (qJson.data.itemBody.questions[i].id == quesValidations[key].responseContainer && quesValidations[key].correctResponse == answerKey[j][0]) {
+                            answerKey[j][2] = "select";
+                            answerKey[j][3] = "correct";
+                        }
                     }
                 })
                 answerKey[j][0] = (parseInt(i) + 1) + "-" + quesOptions[i][j].id;
@@ -149,7 +205,21 @@ function getQuesdetail(qJson, obj) {
         })
         //console.log(obj.answerKey)
     }
-    else if (obj.type == 'fibdropdown-itemplayer') {
+
+    else if (obj.quesType == 'fibtext-itemplayer') {
+        Object.keys(quesOptions).forEach(function (j) {
+            obj.answerKey[j] = [];
+            obj.answerKey[j][0] = quesOptions[j].id;
+            Object.keys(quesValidations).forEach(function (key) {
+                if (quesValidations[key].responseContainer == obj.answerKey[j][0]) {
+                    obj.answerKey[j][1] = quesValidations[key].correctDisplay;
+                    obj.answerKey[j][2] = "correct";
+                }
+            })
+        })
+    }
+
+    else if (obj.quesType == 'fibdropdown-itemplayer') {
         quesRC = qJson.data.itemBody.responseContainer;
         quesOptions = qJson.data.itemBody.options;
         Object.keys(quesRC).forEach(function (j) {
@@ -167,7 +237,91 @@ function getQuesdetail(qJson, obj) {
         })
         //console.log(obj.answerKey)
     }
+    else if (obj.quesType == 'dnd-imagelabel-itemplayer' || obj.quesType == 'dnd-fib-itemplayer') {
+        Object.keys(quesOptions).forEach(function (j) {
+            obj.answerKey[j] = [];
+            obj.answerKey[j][0] = quesOptions[j].id;
+            obj.answerKey[j][1] = null;
+            Object.keys(quesValidations).forEach(function (key) {
+                if (quesValidations[key].correctResponse == quesOptions[j].id) {
+                    obj.answerKey[j][2] = quesValidations[key].responseContainer;
+                    obj.answerKey[j][3] = quesOptions[j].content.data;
+                    obj.answerKey[j][4] = "correct";
+                }
+            })
+        })
+    }
 
+    /*else if (obj.quesType == 'mcmr-itemplayer') {
+        Object.keys(quesOptions).forEach(function (j) {
+            obj.answerKey[j] = [];
+            obj.answerKey[j][0] = quesOptions[j].id;
+            obj.answerKey[j][1] = quesOptions[j].content.data;
+            obj.answerKey[j][2] = "";
+            obj.answerKey[j][3] = "";
+            Object.keys(qJson.data.itemBody.validations).forEach(function (key) {
+                if (quesValidations[key].responseContainer == obj.answerKey[j][0] && quesValidations[key].correctResponse == 'true') {
+                    obj.answerKey[j][2] = "select";
+                    obj.answerKey[j][3] = "correct";
+                }
+            })
+        })
+    }*/
+    else if (obj.quesType == 'orderlist-itemplayer') {
+        Object.keys(quesOptions).forEach(function (j) {
+            obj.answerKey[j] = [];
+            obj.answerKey[j][0] = quesOptions[j].id;
+            obj.answerKey[j][1] = quesOptions[j].content.data;
+            obj.answerKey[j][2] = "";
+            obj.answerKey[j][3] = "";
+            Object.keys(qJson.data.itemBody.validations).forEach(function (key) {
+                if (quesValidations[key].correctResponse == quesOptions[j].id) {
+                    obj.answerKey[j][2] = quesValidations[key].responseContainer;
+                    obj.answerKey[j][3] = "correct";
+                }
+            })
+        })
+    }
+    else if (obj.quesType == 'classify-itemplayer') {
+        obj.answerKey = { "key1": [], "key2": [] }
+        Object.keys(quesOptions).forEach(function (j) {
+            obj.answerKey.key1[j] = [];
+            obj.answerKey.key1[j][0] = quesOptions[j].id;
+            obj.answerKey.key1[j][1] = "";
+            Object.keys(quesValidations).forEach(function (key) {
+                if (quesValidations[key].correctResponse == quesOptions[j].id) {
+                    obj.answerKey.key1[j][2] = quesValidations[key].responseContainer;
+                    obj.answerKey.key1[j][3] = quesOptions[j].content.data;
+                    obj.answerKey.key1[j][4] = "correct";
+                }
+            })
+        })
+        Object.keys(qJson.data.itemBody.responseContainer).forEach(function (j) {
+            obj.answerKey.key2[j] = [];
+            obj.answerKey.key2[j][0] = qJson.data.itemBody.responseContainer[j].id;
+            obj.answerKey.key2[j][1] = qJson.data.itemBody.responseContainer[j].content.data;
+            obj.answerKey.key2[j][2] = "correct";
+        })
+    }
+    else if (obj.quesType == 'matching-itemplayer') {
+        var responseOptions = qJson.data.itemBody.responseContainer;
+        Object.keys(quesOptions).forEach(function (j) {
+            obj.answerKey[j] = [];
+            obj.answerKey[j][2] = quesOptions[j].id;
+            Object.keys(responseOptions).forEach(function (key) {
+                if (quesValidations[key].responseContainer == responseOptions[j].id) {
+                    obj.answerKey[j][1] = responseOptions[key].content.data;
+                }
+            })
+            Object.keys(quesValidations).forEach(function (key) {
+                if (quesValidations[key].correctResponse == quesOptions[j].id) {
+                    obj.answerKey[j][0] = quesValidations[key].responseContainer;
+                    obj.answerKey[j][3] = quesOptions[j].content.data;
+                    obj.answerKey[j][4] = "correct";
+                }
+            })
+        })
+    }
     else {
         console.log("get questiondetail is not available for " + obj.type);
     }
