84b9080ea32ae6513f7e855b247d1a5e0a373633
Rupsi Mehta <rupsi.mehta@comprotechnologies.com>
2022-09-13 12:28:30 +0530

    engage test

--- form.js
+++ form.js
@@ -132,7 +132,8 @@ app.get('/homepage', function (request, response) {
                 }
                 console.log("Running the test : " + testName)
                 var response1 = shell.exec("npm run test -- --appType=difusionExperienceApp --testEnv=production --testExecFile=" + testName + " --browserCapability=desktop-chrome-1920");
-               response.send(response1);
+          console.log(response1)
+                response.send(response1);
             //  console.log(__dirname + "/output/reports/TestReports/index.html")
              // response.sendFile(__dirname + "/output/reports/TestReports/index.html");
             }
